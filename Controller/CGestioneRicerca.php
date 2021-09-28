824e1b12ea79101665a2ad1b90c00b5fb073cae1
PaoloAttardi <81446798+PaoloAttardi@users.noreply.github.com>
2021-09-28 11:23:21 +0200

    Update CGestioneRicerca.php

--- Controller/CGestioneRicerca.php
+++ Controller/CGestioneRicerca.php
@@ -8,7 +8,7 @@ class CGestioneRicerca
 
         $pm = USingleton::getInstance('FPersistentManager');
 
-        $ricette = $pm::load('FRicetta', array(), 'data');
+        $ricette = $pm::load('FRicetta', array(), 'data', 3);
 
         for($i = 0; $i < 3; $i++){ // passo tre ricette nel carosello
             $ricette_home[$i] = $ricette[$i];
@@ -16,12 +16,12 @@ class CGestioneRicerca
             $immagini[$i] = $pm::load('FImmagine', array(['id', '=', $ricette[$i]->getId_immagine()]));
         }
 
-        $ricette = $pm::load('FRicetta', array(), 'valutazione');
+        $ricette_votate = $pm::load('FRicetta', array(), 'valutazione');
 
-        for($i = 3; $i < 7; $i++){ // passo quattro ricette tra le più votate
-            $ricette_home[$i] = $ricette[$i];
-            $autori_ricette[] = $pm::load('FUtente', array(['id', '=', $ricette[$i]->getAutore()]));
-            $immagini[] = $pm::load('FImmagine', array(['id', '=', $ricette[$i]->getId_immagine()]));
+        for($i = 0; $i < 4; $i++){ // passo quattro ricette tra le più votate
+            $ricette_home[$i+3] = $ricette_votate[$i];
+            $autori_ricette[] = $pm::load('FUtente', array(['id', '=', $ricette_votate[$i]->getAutore()]));
+            $immagini[] = $pm::load('FImmagine', array(['id', '=', $ricette_votate[$i]->getId_immagine()]));
         }
 
         $vSearch->showHome($ricette_home, $autori_ricette, $immagini);
